fd99701dc69dac8829f33ed49e6ff6326f3ecc6c
iromaniuk@advisor360.com <irek.romaniuk@gmail.com>
2021-01-25 18:53:21 -0500

    panoramaPushdg updated

--- vars/panoramaPushdg.groovy
+++ vars/panoramaPushdg.groovy
@@ -1,15 +1,22 @@
-pipeline {
-    agent any
-    environment {
-        API_KEY= credentials("${config.panorama_api_key}")
-    }
-    stages { 
-        stage('Push Panorama device groups') {
-            steps {
-                sh """
-                  ansible-playbook -i "localhost," ${config.playbook_path} -e "api_key=$API_KEY ip_address=${config.ip_address} device_group=${config.device_group}"
-                """
+def call (body) {
+   def config = [:]
+   body.resolveStrategy = Closure.DELEGATE_FIRST
+   body.delegate = config
+   body()
+
+    pipeline {
+        agent any
+        environment {
+            API_KEY= credentials("${config.panorama_api_key}")
+        }
+        stages { 
+            stage('Push Panorama device groups') {
+                steps {
+                    sh """
+                    ansible-playbook -i "localhost," ${config.playbook_path} -e "api_key=$API_KEY ip_address=${config.ip_address} device_group=${config.device_group}"
+                    """
+                }
             }
         }
     }
-}
+}    
--- vars/panoramaPushdgDocker.groovy
+++ vars/panoramaPushdgDocker.groovy
@@ -1,19 +1,26 @@
-pipeline {
-    agent {
-        dockerfile {
-            dir "${config.dockerfilePath}"
+def call (body) {
+   def config = [:]
+   body.resolveStrategy = Closure.DELEGATE_FIRST
+   body.delegate = config
+   body()
+
+    pipeline {
+        agent {
+            dockerfile {
+                dir "${config.dockerfilePath}"
+            }
         }
-    }
-    environment {
-        API_KEY= credentials("${config.panorama_api_key}")
-    }
-    stages { 
-        stage('Push Panorama device groups') {
-            steps {
-                sh """
-                  ansible-playbook -i "localhost," ${config.playbook_path} -e "api_key=$API_KEY ip_address=${config.ip_address} device_group=${config.device_group}"
-                """
+        environment {
+            API_KEY= credentials("${config.panorama_api_key}")
+        }
+        stages { 
+            stage('Push Panorama device groups') {
+                steps {
+                    sh """
+                    ansible-playbook -i "localhost," ${config.playbook_path} -e "api_key=$API_KEY ip_address=${config.ip_address} device_group=${config.device_group}"
+                    """
+                }
             }
         }
     }
-}
+}    
