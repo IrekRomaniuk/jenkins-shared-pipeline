95685350b8c592b22e72c294052b24e4a18165bc
iromaniuk@advisor360.com <irek.romaniuk@gmail.com>
2021-01-25 17:50:03 -0500

    panoramaPushdg added

---
+++ vars/panoramaPushdg.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent agent any
+    environment {
+        API_KEY= credentials("${config.panorama_api_key}")
+    }
+    stages { 
+        stage('Push Panorama device groups') {
+            steps {
+                sh """
+                  ansible-playbook -i "localhost," ${config.playbook_path} -e "api_key=$API_KEY ip_address=${config.ip_address} device_group=${config.device_group}"
+                """
+            }
+        }
+    }
+}
